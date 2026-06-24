6fb76b0e7ff08abbc27717edc2d482b7732690d6
LY <734769114@qq.com>
2026-06-24 16:54:19 +0800

    Remove unnecessary folders and files

--- test/py_api/golden_cmp/test.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-python3 golden_cmp/golden_cmp.py -i tadd
-python3 golden_cmp/golden_cmp.py -i tsub
-python3 golden_cmp/golden_cmp.py -i texp
-python3 golden_cmp/golden_cmp.py -i tmax
--- test/tileop_api/linxStartEnd.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LINX_START_END_H
-#define LINX_START_END_H
-
-#define PMC_START() __asm__ __volatile__("B.HINT TRACE.begin\n" : : :);
-
-#define PMC_END() __asm__ __volatile__("B.HINT TRACE.end\n" : : :);
-
-#endif
