fdce0cf1fd2630768853217a3f2d7ac1034871fb
LY <734769114@qq.com>
2026-06-24 16:46:30 +0800

    Remove include, models folders and Makefile

--- include/accelerator/v310/constants.hpp
+++
@@ -1,7 +0,0 @@
-constexpr int REPEAT_BYTE = 256;
-
-constexpr int REPEAT_MAX = 255;
-
-constexpr const int BLOCK_SIZE = 32;
-
-constexpr const int REPEAT_STRIDE_MAX = 255;
--- include/jcore/constants.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef CONSTANTS_HPP
-#define CONSTANTS_HPP
-constexpr uint16_t LaneNum = 64;
- 
-#endif
